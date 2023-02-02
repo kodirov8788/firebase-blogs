5bf1be6f512886250e9675a51941a4eae27913dd
kodirov8788 <kodirov8788@gmail.com>
2023-02-02 05:12:58 +0500

    adsasd

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Admin from './page/Admin';
-import AdminMultiple from './page/AdminMultiple';
-import MultipleImage from './MultipleImage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,9 +11,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='/images' element={<MultipleImage />} />
         <Route path='admin' element={<Admin />} />
-        <Route path='adminfiles' element={<AdminMultiple />} />
       </Routes>
     </BrowserRouter>
 
